083197c07d130cf0a5da5136c756e7a22b226a00
Shannon Millar <shanenak@gmail.com>
2023-10-23 14:43:17 -0700

    Add validations to user model

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,6 +11,9 @@ const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
 
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.json({
@@ -19,7 +22,7 @@ router.get('/', function (req, res, next) {
 });
 
 // POST /api/users/register
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
@@ -61,7 +64,7 @@ router.post('/register', async (req, res, next) => {
   });
 });
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
---
+++ backend/validations/register.js
@@ -0,0 +1,27 @@
+const { check } = require("express-validator");
+const handleValidationErrors = require('./handleValidationErrors');
+
+// validateRegisterInput is a combination Express middleware that uses the 
+// `check` middleware to validate the keys in the body of a request to 
+// register a user
+const validateRegisterInput = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Email is invalid'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 30 })
+    .withMessage('First name must be between 1 and 30 characters'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Last name must be between 1 and 30 characters'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 8, max: 30 })
+    .withMessage('Password must be between 8 and 30 characters'),
+  handleValidationErrors
+];
+
+module.exports = validateRegisterInput;
