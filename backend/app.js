f9f72ea391d0a0be33e224cc59f0ae0dff7f24b0
Shannon Millar <shanenak@gmail.com>
2023-10-23 14:17:58 -0700

    Finish login and register endpoints

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User');
+require('./config/passport');
+const passport = require('passport'); 
+
 const usersRouter = require('./routes/api/users');
 const csrfRouter = require('./routes/api/csrf');
-
 
 const app = express();
 
@@ -17,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
 // Security Middleware
 if (!isProduction) {
