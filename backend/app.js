dacfc14ff46c386c747828a20e366cbe17f0193c
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-23 12:13:15 -0700

    configure csrf protection

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,43 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var usersRouter = require('./routes/api/users');
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
-var app = express();
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
+
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
 app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
