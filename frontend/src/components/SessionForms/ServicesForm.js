31b55e1703e4679ccbbd57885e5f30674cd06575
Julia Kahn <juliakahn1@gmail.com>
2023-10-23 17:43:21 -0700

    Add skip and submit buttons to form

--- frontend/src/components/SessionForms/ServicesForm.js
+++ frontend/src/components/SessionForms/ServicesForm.js
@@ -2,21 +2,37 @@ import { useDispatch } from "react-redux"
 import { useState } from "react"
 
 const ServicesForm = () => {
-  const [service, setService] = useState('')
+  const [serviceCategory, setServiceCategory] = useState('')
   const [portfolio, setPortfolio] = useState('')
   const services = ['Photography', 'Bartending', 'Gardening']
+
+  console.log(serviceCategory)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const service = {
+      serviceCategory,
+      portfolio
+    }
+    // dispatch
+  }
 
 return (
   <>
     <div className="profile-services-form">
       <h2>Profile details</h2>
       <h3>Services</h3>
-      <form>
+      <p>If you're looking to offer services, select what best describes your skills and a link to your portfolio, if you have one.</p>
+      <form onSubmit={handleSubmit}>
         <div className="services-tiles">
-          { services.map(service => {
+          { services.map((service, index) => {
             return (
-              <div className="service-input-container">
-                <input value={service} className="service-radio-button" type="radio" name="radio"/>
+              <div key={index} className="service-input-container">
+                <input value={service}
+                  className="service-radio-button"
+                  type="radio"
+                  name="radio"
+                  onChange={() => setServiceCategory({service})}/>
                   <div className="radio-tile">{service}</div>
               </div>
             )
@@ -27,13 +43,17 @@ return (
           <input type="text"
             value={portfolio}
             onChange={(e) => setPortfolio(e.currentTarget.value)}>
-
           </input>
         </label>
+        <input
+          type="submit"
+          value="Add services"
+          disabled={!serviceCategory || !portfolio}
+        />
       </form>
+      <button>Skip</button>
     </div>
-      {/* <button onClick={() => updateProfile(e)}>Create Account</button>
-    <button>Skip</button> */}
+
   </>
 )
 }
