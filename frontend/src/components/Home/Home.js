baf3b465931b4e2ee86207cb50f930dd76c31014
Julia Kahn <juliakahn1@gmail.com>
2023-10-24 15:26:21 -0700

    Add store subscriber from Home.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,17 @@
+import { useDispatch,useSelector } from "react-redux";
+import React from "react";
+import { useEffect } from "react";
+import { fetchServices } from "../../store/services";
+
 const Home = () => {
   // get all services from the store/backend with useEffect and useSelector
+  const dispatch = useDispatch()
+  const allServices = useSelector(store => store.services)
+
+  useEffect(() => {
+    dispatch(fetchServices())
+  }, [])
+
   return (
     <>
       <p>Amateur Hour homepage</p>
