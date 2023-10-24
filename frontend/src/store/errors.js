aaf397df69edc71e7fad5359c35df4ee24f661ba
Shannon Millar <shanenak@gmail.com>
2023-10-24 16:31:31 -0700

    Add actions for jobs reducer

--- frontend/src/store/errors.js
+++ frontend/src/store/errors.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { sessionErrorsReducer } from './session';
+import { jobsErrorsReducer } from './jobs';
 
 export default combineReducers({
-  session: sessionErrorsReducer
+  session: sessionErrorsReducer,
+  jobs: jobsErrorsReducer
   // will also have jobs
 });
--- frontend/src/store/jobs.js
+++ frontend/src/store/jobs.js
@@ -1,10 +1,13 @@
 import jwtFetch from "./jwt"
+import { getService } from "./services";
 
 const GET_JOB = "jobs/GET_JOB";
 const GET_JOBS = "jobs/GET_JOBS";
 const ADD_JOB = "jobs/ADD_JOB";
 const EDIT_JOB = "jobs/EDIT_JOB";
 const REMOVE_JOB = "jobs/REMOVE_JOB";
+const GET_JOB_ERRORS = "jobs/GET_JOB_ERRORS";
+const CLEAR_JOB_ERRORS = "jobs/CLEAR_JOB_ERRORS";
 
 const getJob = job => {
   return {
@@ -41,69 +44,216 @@ const removeJob = jobId => {
   }
 }
 
-export const fetchJob = (jobId) => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/${jobId}`)
-  if (res.ok) {
-    const job = await res.json()
-    dispatch(getJob(job))
-  }
+const getJobErrors = errors => {
+    return {
+        type: GET_JOB_ERRORS,
+        errors
+    }
 }
 
+export const clearJobErrors = errors => {
+    return {
+        type: CLEAR_JOB_ERRORS,
+        errors
+    }
+}
+
+export const fetchJob = (jobId) => async (dispatch) => {
+    try {
+        const res = await jwtFetch(`/api/jobs/${jobId}`)
+        if (res.ok) {
+            const job = await res.json()
+            dispatch(getService(job.service._id))
+            const payload = {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            dispatch(getJob(payload))
+        }
+    } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
+};
+
 export const fetchJobs = () => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/`)
-  if (res.ok) {
-    const jobs = await res.json()
-    dispatch(getJobs(jobs))
-  }
+    try {
+        const res = await jwtFetch(`/api/jobs/`)
+        if (res.ok) {
+            const jobs = await res.json()
+            jobs.forEach((job)=> {
+                dispatch(getService(job.service._id))
+            })
+            const payload = jobs.map((job)=>{
+                return {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            })
+            dispatch(getJobs(payload))
+        }
+     } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
 
 export const fetchClientJobs = (clientId) => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/client/${clientId}`)
-  if (res.ok) {
-    const jobs = await res.json()
-    dispatch(getJobs(jobs))
-  }
+    try {
+        const res = await jwtFetch(`/api/jobs/client/${clientId}`)
+        if (res.ok) {
+            const jobs = await res.json()
+            jobs.forEach((job)=> {
+                dispatch(getService(job.service._id))
+            })
+            const payload = jobs.map((job)=>{
+                return {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            })
+            dispatch(getJobs(payload))
+        }
+     } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
 
 export const fetchProviderJobs = (providerId) => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/provider/${providerId}`)
-  if (res.ok) {
-    const jobs = await res.json()
-    dispatch(getJobs(jobs))
-  }
+    try {
+        const res = await jwtFetch(`/api/jobs/provider/${providerId}`)
+        if (res.ok) {
+            const jobs = await res.json()
+            jobs.forEach((job)=> {
+                dispatch(getService(job.service._id))
+            })
+            const payload = jobs.map((job)=>{
+                return {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            })
+            dispatch(getJobs(payload))
+        }
+     } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
 
 export const createJob = (job) => async (dispatch) => {
-  const res = await jwtFetch('/api/jobs/', {
-    method: 'POST',
-    body: JSON.stringify(job)
-  })
-  if (res.ok) {
-    const job = await res.json()
-    dispatch(addJob(job))
-  }
+    try {
+        const res = await jwtFetch('/api/jobs/', {
+            method: 'POST',
+            body: JSON.stringify(job)
+        })
+        if (res.ok) {
+            const job = await res.json()
+            dispatch(getService(job.service._id))
+            const payload = {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            dispatch(addJob(payload))
+        }
+    } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
 
 export const updateJob = (job, jobId) => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/${jobId}`, {
-    method: 'PATCH',
-    body: JSON.stringify(job)
-  })
-  if (res.ok) {
-    const job = await res.json()
-    dispatch(editJob(job))
-  }
+    try {
+        const res = await jwtFetch(`/api/jobs/${jobId}`, {
+            method: 'PATCH',
+            body: JSON.stringify(job)
+        })
+        if (res.ok) {
+            const job = await res.json()
+            dispatch(getService(job.service._id))
+            const payload = {
+                    _id: job._id,
+                    service: job.service._id,
+                    provider: job.service.provider,
+                    client: job.client._id,
+                    statusDescription: job.statusDescription,
+                    date: job.date,
+                    description: job.description
+                }
+            dispatch(editJob(payload))
+        }
+    } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
 
 export const deleteJob = (jobId) => async (dispatch) => {
-  const res = await jwtFetch(`/api/jobs/${jobId}`, {
-    method: 'DELETE',
-  })
-  if (res.ok) {
-    const job = await res.json()
-    dispatch(removeJob(job._id))
-  }
+    try {
+        const res = await jwtFetch(`/api/jobs/${jobId}`, {
+          method: 'DELETE',
+        })
+        if (res.ok) {
+          const job = await res.json()
+          dispatch(removeJob(job._id))
+        }
+    } catch (err) {
+        const resBody = await err.json();
+        if (resBody.statusCode === 400) {
+            dispatch(getJobErrors(resBody.errors));
+        }
+    }
 }
+
+const nullErrors = null;
+export const jobsErrorsReducer = (state = nullErrors, action) => {
+    switch(action.type) {
+        case GET_JOB_ERRORS:
+            return action.errors;
+        case CLEAR_JOB_ERRORS:
+            return nullErrors;
+        default:
+            return state;
+    }
+};
 
 export const jobsReducer = (store = {}, action) => {
   const newStore = { ...store }
