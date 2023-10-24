54cc876569029ba65f0f5efac5e25db9fa663968
Shannon Millar <shanenak@gmail.com>
2023-10-23 21:09:18 -0700

    Update constants.js to include status descriptions

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
 const compOptions = ['Yelp review', 'Google review', 'Instagram post and tag account']
 const serviceCategories = ['photography', 'gardening', 'art', 'modeling']
+const statusOptions = ['requested', 'accepted', 'providerCompleted', 'clientCompleted', 'providerCompensated']
 
-module.exports = { compOptions, serviceCategories }
+module.exports = { compOptions, serviceCategories, statusOptions }
