41565676802892d8db56b5420209f18efd82ace2
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-25 10:26:37 -0700

    begin to implement populating DashboardItemJobIndex

--- frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
+++ frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
@@ -1,6 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './DashboardItemJobIndex.css';
 
 const DashboardItemJobIndex = ({indexType}) => {
+    // get all jobs from the store/backend with useEffect and useSelector
+    const dispatch = useDispatch();
+    const allJobs = Object.values(useSelector(store => store.jobs));
+
     return (
         <>
             <div className="dashboard-item-job-index-container">
