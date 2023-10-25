944adafa54b204d62391533de549cc1f2ccfc270
Julia Kahn <juliakahn1@gmail.com>
2023-10-24 20:08:53 -0700

    Style tiles for users/services

--- frontend/src/components/Home/ServiceItem.js
+++ frontend/src/components/Home/ServiceItem.js
@@ -7,14 +7,24 @@ const ServiceItem = ({service}) => {
       <div className="service-item-shape">
         <div className="service-item-inner-container">
           <div className="service-item-metadata">
-            <h2 className="service-item-name">{service.provider.firstName}</h2>
-            <h3 className="service-item-category">{service.category}</h3>
-            <div className="compensation-container">
+            <div>
+              <header className="service-item-header">
+                <h3 className="service-item-category">{service.category}</h3>
+                <h2 className="service-item-name">{service.provider.firstName}</h2>
+              </header>
+              <section className="service-item-details">
+                <span className="service-tile-portfolio">View portfolio</span>
+                <span className="service-tile-job-count">12 jobs completed</span>
+                <span className="service-tile-compensation-label">Compensation</span>
+                <span className="service-tile-compensation-data">{service.compensation}</span>
+              </section>
+              <button className="service-item-button">Book</button>
+            </div>
+            {/* <div className="compensation-container">
               <span className="compensation-label">Compensation</span>
               <span>{service.compensation}</span>
-            </div>
+            </div> */}
           </div>
-          <button className="service-item-button">Book</button>
         </div>
       </div>
     </>
