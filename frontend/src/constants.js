789a33f40b7d564c54cea902775d65202286f977
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-25 09:24:00 -0700

    add to constants arrays

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,19 @@
-const compOptions = ['Yelp review', 'Google review', 'Instagram post and tag account']
-const serviceCategories = ['photography', 'gardening', 'art', 'modeling']
+const compOptions = [
+    'Yelp review',
+    'Google review',
+    'Instagram post and tag account'
+]
+const serviceCategories = [
+    'photography',
+    'gardening',
+    'art',
+    'modeling',
+    'cleaning',
+    'bartending',
+    'dj',
+    'moving',
+    'videography',
+    'decorating'
+]
 
 module.exports = { compOptions, serviceCategories }
