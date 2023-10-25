55533a490b301cf22430319044f0ca4f5f23b33b
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-25 11:53:11 -0700

    implement initial rendering of requested jobs and 'provided' jobs

--- frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
+++ frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
@@ -6,7 +6,14 @@ import './DashboardItemJobIndex.css';
 const DashboardItemJobIndex = ({indexType}) => {
     // get all jobs from the store/backend with useEffect and useSelector
     const dispatch = useDispatch();
+    const currentUser = useSelector(store => store.session.user);
     const allJobs = Object.values(useSelector(store => store.jobs));
+    const services = useSelector(state => state.services);
+
+    let specificJobs
+    indexType === "requests" ?
+        specificJobs = allJobs.filter(job => job.client._id === currentUser._id) : 
+        specificJobs = allJobs.filter(job => job.provider._id === currentUser._id);
 
     useEffect(() => {   
         if (allJobs.length === 0) dispatch(fetchJobs())
@@ -16,6 +23,17 @@ const DashboardItemJobIndex = ({indexType}) => {
         <>
             <div className="dashboard-item-job-index-container">
                 <div className="dashboard-item-header">{indexType}</div>
+                <div className="job-index-container">
+                    {specificJobs.map(job => {
+                        return (
+                            <div key={job._id}>
+                                <div>{indexType === "requests" ? job.provider.firstName : job.client.firstName}</div>
+                                <div>{job.description}</div>
+                                <div>{job.statusDescription}</div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </>
     );
