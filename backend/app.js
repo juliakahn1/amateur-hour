ce62d7d0b61a05801b0472b9887b31f0e9b66d8e
Shannon Millar <shanenak@gmail.com>
2023-10-25 10:37:57 -0700

    Serve up CSRF token for production

--- backend/app.js
+++ backend/app.js
@@ -51,6 +51,29 @@ app.use('/api/services', servicesRouter);
 app.use('/api/jobs', jobsRouter);
 app.use('/api/csrf', csrfRouter);
 
+// Serve static React build files statically in production
+if (isProduction) {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+        );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+        );
+    });
+}
+
 // Express custom middleware for catching all unmatched requests and formatting
 // a 404 error to be sent as the response.
 app.use((req, res, next) => {
