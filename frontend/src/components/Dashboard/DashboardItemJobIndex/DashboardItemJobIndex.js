fbf76a6f58bc2c6f0008cef3226840954dacd94f
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-25 10:39:24 -0700

    implement dispatch of fetchJobs to update store

--- frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
+++ frontend/src/components/Dashboard/DashboardItemJobIndex/DashboardItemJobIndex.js
@@ -1,10 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchJobs } from '../../../store/jobs';
 import './DashboardItemJobIndex.css';
 
 const DashboardItemJobIndex = ({indexType}) => {
     // get all jobs from the store/backend with useEffect and useSelector
     const dispatch = useDispatch();
     const allJobs = Object.values(useSelector(store => store.jobs));
+
+    useEffect(() => {   
+        if (allJobs.length === 0) dispatch(fetchJobs())
+    }, []);
 
     return (
         <>
