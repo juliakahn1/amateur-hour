359f254678f96cb62b57e7123f9c31e41e47c6af
Julia Kahn <juliakahn1@gmail.com>
2023-10-25 15:16:29 -0700

    Add logic to prevent redirect if user didn't sign in correctly

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { getCurrentUser } from '../../store/session';
 import './SessionForm.scss';
+
 
 import { login, clearSessionErrors } from '../../store/session';
 
@@ -9,8 +11,10 @@ function LoginForm () {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(state => state.errors.session);
+  const currentUser = useSelector(state => state.session?.user);
   const dispatch = useDispatch();
   const history = useHistory()
+  console.log(currentUser)
 
   useEffect(() => {
     return () => {
@@ -25,8 +29,8 @@ function LoginForm () {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(login({ email, password }));
-    history.push('/')
+    dispatch(login({ email, password }))
+    return currentUser ? history.push('/') : null
   }
 
   const toSignUp = (e) => {
@@ -36,47 +40,49 @@ function LoginForm () {
 
   return (
     <>
-      <div className="session-page-container login-page">
-        <div className="session-page-inner-container login-page">
-          {/* <p>
-            A description about the gist of the site and its purpose goes here.
-            Includes mission statement and aim of use.
-          </p> */}
-          <h2 className="session-form-title login-form">Log in to book new services and see your ongoing jobs</h2>
-          <form className="session-form login-form" onSubmit={handleSubmit}>
-            <div className="session-input-container">
-              <span className="session-input-label">Email</span>
-              <input type="text"
-                value={email}
-                onChange={update('email')}
-                placeholder="Email"
-                className="session-input-text-field"
+      <div className="session-page-background">
+        <div className="session-page-container login-page">
+          <div className="session-page-inner-container login-page">
+            {/* <p>
+              A description about the gist of the site and its purpose goes here.
+              Includes mission statement and aim of use.
+            </p> */}
+            <h2 className="session-form-title login-form">Log in to book new services and see your ongoing jobs</h2>
+            <form className="session-form login-form" onSubmit={handleSubmit}>
+              <div className="session-input-container">
+                <span className="session-input-label">Email</span>
+                <input type="text"
+                  value={email}
+                  onChange={update('email')}
+                  placeholder="Email"
+                  className="session-input-text-field"
+                />
+              <div className="session-errors">{errors?.email}</div>
+              </div>
+              <div className="session-input-container">
+                <span className="session-input-label">Password</span>
+                <input type="password"
+                  value={password}
+                  onChange={update('password')}
+                  placeholder="Password"
+                  className="session-input-text-field"
+                />
+              <div className="session-errors">{errors?.password}</div>
+              </div>
+              <input
+                type="submit"
+                value="Log In"
+                disabled={!email || !password}
+                className="session-form-button"
               />
-            <div className="session-errors">{errors?.email}</div>
-            </div>
-            <div className="session-input-container">
-              <span className="session-input-label">Password</span>
-              <input type="password"
-                value={password}
-                onChange={update('password')}
-                placeholder="Password"
-                className="session-input-text-field"
-              />
-            <div className="session-errors">{errors?.password}</div>
-            </div>
-            <input
-              type="submit"
-              value="Log In"
-              disabled={!email || !password}
-              className="session-form-button"
-            />
-          <button className="session-form-button create-account"
-            onClick={toSignUp}>
-            Create an AmateurHour account
-          </button>
-          </form>
+            <button className="session-form-button create-account"
+              onClick={toSignUp}>
+              Create an AmateurHour account
+            </button>
+            </form>
+          </div>
         </div>
-        </div>
+      </div>
     </>
   );
 }
