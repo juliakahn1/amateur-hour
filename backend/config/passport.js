52808be1a18a8e6d4ecbd87a6f1714ba3bd733b7
Shannon Millar <shanenak@gmail.com>
2023-10-26 10:52:35 -0700

    Add firstName and lastName to loginUser

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -25,7 +25,10 @@ passport.use(new LocalStrategy({
 exports.loginUser = async function(user) {
   const userInfo = {
     _id: user._id,
-    email: user.email
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    location: user.location
   };
   const token = await jwt.sign(
     userInfo, // payload
