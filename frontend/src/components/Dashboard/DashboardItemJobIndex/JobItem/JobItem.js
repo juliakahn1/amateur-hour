48ea9196e1150940418ecdee2132491d023e013d
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-25 17:35:48 -0700

    add job status rendering to JobItem; limit status advance button to question statuses

--- frontend/src/components/Dashboard/DashboardItemJobIndex/JobItem/JobItem.js
+++ frontend/src/components/Dashboard/DashboardItemJobIndex/JobItem/JobItem.js
@@ -1,12 +1,25 @@
-import { requestedJobStatuses, providedJobStatuses } from '../../../../constants';
-
+import { useDispatch } from 'react-redux';
+import { requestedJobStatuses, providedJobStatuses, statusOptions } from '../../../../constants';
 import './JobItem.scss';
 
 const JobItem = ({ indexType, job, name, service = {}, email }) => {
+    const dispatch = useDispatch();
+
     const date = new Date(job.date);
     const status = indexType === "Requested" ?
         requestedJobStatuses[job.statusDescription] :
         providedJobStatuses[job.statusDescription];
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const statusIndex = statusOptions.indexOf(job.statusDescription);
+        if (statusIndex < statusOptions.length - 1) {
+            console.log('next status: ', statusOptions[statusIndex + 1])
+        } else {
+            console.log('final status: ', statusOptions[statusIndex])
+        }
+    }
+    
     return (
         <div className="job-item-container">
             <div className="job-item-header">{name} - {service.category}</div>
@@ -17,7 +30,7 @@ const JobItem = ({ indexType, job, name, service = {}, email }) => {
                 {status}
                 {status.includes("?") ?
                     <div className="job-item-status-buttons">
-                        <button>Yes</button>
+                        <button onClick={handleClick}>Yes</button>
                     </div> : <></>}
             </div>
         </div>
