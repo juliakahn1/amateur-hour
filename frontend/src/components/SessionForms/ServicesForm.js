b0b7ed360820d9fbbddc01be67fbbd34e994a4da
Julia Kahn <juliakahn1@gmail.com>
2023-10-26 10:18:50 -0700

    Add background and containers

--- frontend/src/components/SessionForms/ServicesForm.js
+++ frontend/src/components/SessionForms/ServicesForm.js
@@ -32,57 +32,64 @@ const ServicesForm = () => {
 
 return (
   <>
-    <div className="profile-services-form">
-      <h2>Profile details</h2>
-      <h3>Services</h3>
-      <p>If you're looking to offer services, select what best describes your skills and a link to your portfolio, if you have one.</p>
-      <form onSubmit={handleSubmit}>
-        <h4>Your skills</h4>
-        <div className="services-tiles">
-          { serviceCategories.map((service, index) => {
-            return (
-              <div key={index} className="service-input-container">
-                <input value={service}
-                  className="service-radio-button"
-                  type="radio"
-                  name="radio-service"
-                  onChange={() => setServiceCategory(service)}/>
-                  <div className="radio-tile">{service}</div>
-              </div>
-            )
-          })}
-        </div>
-        <label>
-          <span>Portfolio link</span>
-          <input type="text"
-            value={portfolio}
-            onChange={(e) => setPortfolio(e.currentTarget.value)}>
-          </input>
-        </label>
-        <h4>Choose your compensation</h4>
-        <div className="services-tiles">
-          { compOptions.map((comp, index) => {
-            return (
-              <div key={index} className="service-input-container">
-                <input value={comp}
-                  className="service-radio-button"
-                  type="radio"
-                  name="radio-comp"
-                  onChange={() => setCompensation(comp)}/>
-                  <div className="radio-tile">{comp}</div>
-              </div>
-            )
-          })}
-        </div>
-        <input
-          type="submit"
-          value="Add services"
-          disabled={!serviceCategory || !portfolio || !compensation}
-        />
-      </form>
-      <button onClick={(e) => skipServices(e)}>Skip</button>
-    </div>
+    <div className="sign-services-background">
 
+      <div className="signup-services-container">
+        <div className="signup-services-inner-container">
+          <h2 className="signup-services-title">Looking to gain cred for your skills?</h2>
+          <h3 className="signup-services-byline">Add services you're offering, a link to your portfolio of work, and how you'd like to be compensated by your clients. You can change all of this info on your profile anytime.</h3>
+          <div className="signup-services-form">
+            <h3>Services</h3>
+            <p>If you're looking to offer services, select what best describes your skills and a link to your portfolio, if you have one.</p>
+            <form onSubmit={handleSubmit}>
+              <h4>Your skills</h4>
+              <div className="services-tiles">
+                { serviceCategories.map((service, index) => {
+                  return (
+                    <div key={index} className="service-input-container">
+                      <input value={service}
+                        className="service-radio-button"
+                        type="radio"
+                        name="radio-service"
+                        onChange={() => setServiceCategory(service)}/>
+                        <div className="radio-tile">{service}</div>
+                    </div>
+                  )
+                })}
+              </div>
+              <label>
+                <span>Portfolio link</span>
+                <input type="text"
+                  value={portfolio}
+                  onChange={(e) => setPortfolio(e.currentTarget.value)}>
+                </input>
+              </label>
+              <h4>Choose your compensation</h4>
+              <div className="services-tiles">
+                { compOptions.map((comp, index) => {
+                  return (
+                    <div key={index} className="service-input-container">
+                      <input value={comp}
+                        className="service-radio-button"
+                        type="radio"
+                        name="radio-comp"
+                        onChange={() => setCompensation(comp)}/>
+                        <div className="radio-tile">{comp}</div>
+                    </div>
+                  )
+                })}
+              </div>
+              <input
+                type="submit"
+                value="Add services"
+                disabled={!serviceCategory || !portfolio || !compensation}
+              />
+            </form>
+            <button onClick={(e) => skipServices(e)}>Skip</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </>
 )
 }
